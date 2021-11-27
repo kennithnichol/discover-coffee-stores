7dc165428c093ed244c6107ca97f0a1e1f48816c
kennithnichol <1321022+kennithnichol@users.noreply.github.com>
2021-11-27 00:13:45 -0800

    update styles

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,19 +1,21 @@
 import styles from './banner.module.css';
 
 const Banner = (props: any) => {
-    const {buttonText, handler} = props;
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>
-        <span>Coffee</span>
-        <span>Connoisseurs</span>
-      </h1>
-      <p className={styles.subtitle}>Discover your local coffee shops!</p>
-      <div className="buttonWrapper">
-        <button onClick={handler}>{buttonText}</button>
-      </div>
-    </div>
-  );
+	const { buttonText, handler } = props;
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>
+				<span className={styles.title1}>Coffee</span>
+				<span className={styles.title2}>Connoisseurs</span>
+			</h1>
+			<p className={styles.subTitle}>Discover your local coffee shops!</p>
+			<div className={styles.buttonWrapper}>
+				<button className={styles.button} onClick={handler}>
+					{buttonText}
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
