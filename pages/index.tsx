90bfc70e2d5e75a6234d14caca821ecf7bcba221
kennithnichol <1321022+kennithnichol@users.noreply.github.com>
2021-11-27 00:54:50 -0800

    add classnames lib and style store Card

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Banner from '../components/banner';
+import Card from '../components/card';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -16,6 +17,13 @@ const Home: NextPage = () => {
 				<Banner buttonText="View nearby stores" />
 				<div className={styles.heroImage}>
 					<Image src="/vercel.svg" width={700} height={400} />
+				</div>
+				<div className={styles.cardLayout}>
+					<Card
+						name="DarkHorse Coffee"
+						imgUrl="/vercel.svg"
+						href="/coffee-store/darkhorse-coffee"
+					/>
 				</div>
 			</main>
 		</div>
